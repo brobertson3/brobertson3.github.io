a8b142dc333efba8304cae1899cd508094294a3b
brobertson3 <TalkNerdyToMe@Brents-MacBook-Pro.local>
2016-05-17 10:49:16 -0400

    Changes before I try text transitions
    Added background images to the re_pane pages.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
 	
+ //  	$(function () {
+ // 		$('[data-toggle="popover"]').popover();
+	// });
+	
+
 	/* Makes the bottle draggable */
 	$('#draggable_bottle').draggable({
 		containment: 'div.container-fluid', //contain in this container so won't mess up ui
@@ -19,25 +24,4 @@ $(document).ready(function () {
 		/* redirect to the new page based on the image it was dragged to */
 		window.location.href = '' + item + ".html";
 	}
-
-	// /* Function to be called when the element is dropped. */
-	// document.drop = function (event) {
-	// 	event.preventDefault();
-	// 	/* Make sure it's the bottle dropping in case other elements can later on */
- //    	var data = event.dataTransfer.getData("text");
- //    	console.log("You're going to the reduce page");
-
-	// }
-
-	//  Prevent the default action and allow the image to be dropped on 
-	//    another element. 
-	// document.allowDrop = function (event) {
-	// 	event.preventDefault();
-	// }
-
-	// /* Function when the element is dragged. */
-	// document.drag = function (event) {
-	// 	event.dataTransfer.setData("text", event.target.id);
-	// 	console.log("" + event.target.id);
-	// }
 });
